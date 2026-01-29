4007614b4f21347ca4a1051fce50b41f6b5924a3
JpMayor1 <royamsemaj@gmail.com>
2026-01-29 09:49:44 +0800

    ui: implemented register page

---
+++ src/components/input/CustomInput.tsx
@@ -0,0 +1,23 @@
+interface CustomInputI {
+  label: string;
+  type?: string;
+  parentClass?: string;
+  inputClass?: string;
+}
+
+export default function CustomInput({
+  parentClass,
+  label,
+  type = "string",
+  inputClass,
+}: CustomInputI) {
+  return (
+    <div className={`w-65 flex flex-col ${parentClass}`}>
+      <label className="text-base">{label}</label>
+      <input
+        type={type}
+        className={`bg-primary rounded-sm text-white px-4 py-2 border-0 outline-0 ${inputClass}`}
+      />
+    </div>
+  );
+}
